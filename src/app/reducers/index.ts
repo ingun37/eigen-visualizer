d1d4943f4ff859c6578421817268f549fae43274
ingun <ingun37@gmail.com>
2020-02-25 16:53:13 +0900

    refactory

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -10,7 +10,8 @@ import {
   on
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import { Matrix4, Matrix3 } from 'three';
+import { Matrix4, Matrix3, Object3D } from 'three';
+import * as THREE from 'three';
 import Matrix, { EigenvalueDecomposition, determinant } from 'ml-matrix'
 
 export class State {
@@ -72,22 +73,12 @@ export const selectThreeMatrix = createSelector(selectMatrix, selectInterpolatio
     m3[1][0], m3[1][1], m3[1][2], 0,
     m3[2][0], m3[2][1], m3[2][2], 0,
     0, 0, 0, 1)
-  let mi = new Matrix4().identity()
-  
   return m4
 })
 
 export const selectEigen = createSelector(selectMatrix, (matrixState) => {
   return new EigenvalueDecomposition(matrixState.m33)
 })
-
-export class Everything {
-  constructor(
-    public matrix: Matrix4,
-    public ei: EigenvalueDecomposition,
-    public sh: Shape,
-  ) { }
-}
 
 export const selectDeterminant = createSelector(selectMatrix, (matrixState) => {
   return determinant(matrixState.m33)
@@ -96,6 +87,82 @@ export const selectDeterminant = createSelector(selectMatrix, (matrixState) => {
 export const selectShape = (state: State) => state.shape
 
 
-export const selectEverything = createSelector(selectThreeMatrix, selectEigen, selectShape, (mat, ei, sh) => {
-  return new Everything(mat, ei, sh)
+export const selectEigenVectors = createSelector(selectEigen, (eigen)=>{
+  return [0,1,2].map(i=>eigen.eigenvectorMatrix.getColumn(i)).map(c=>new THREE.Vector3(c[0],c[1],c[2]))
 })
+
+export const selectShapeModel = createSelector(selectThreeMatrix, selectShape, selectEigenVectors, (mat, sh, eVecs)=>{
+  let o:THREE.Object3D
+  if (sh == Shape.Cube) {
+    o = makeCube()
+  } else {
+    o = makeSphere(eVecs[0])
+  }
+  o.applyMatrix4(mat)
+  return o
+})
+
+export const selectEigenVectorModels = createSelector(selectEigenVectors, (eVecs)=>{
+  return eVecs.map(v=>makeVector(v, 0xff0000))
+})
+
+export const selectModels = createSelector(selectShapeModel, selectEigenVectorModels, (tm, em)=>{
+  return em.concat([tm])
+})
+
+function makeCube(): THREE.Object3D {
+
+  var geometry = new THREE.BoxGeometry().translate(0.5, 0.5, 0.5);
+  var material = new THREE.MeshBasicMaterial({
+    color: 0x70a0d0,
+    transparent: true,
+    opacity: 0.5
+  });
+  var cube = new THREE.Mesh(geometry, material);
+
+  var frameMat = new THREE.LineBasicMaterial({ color: 0x112233, linewidth: 3 });
+  let geo = new THREE.Geometry()
+  geo.vertices = [[0, 1, 0], [1, 1, 0], [1, 1, 0], [1, 1, 1], [1, 1, 1], [0, 1, 1], [0, 1, 1], [0, 1, 0],
+  [0, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 1], [1, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 0],
+  [0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 1, 1]].map(x => new THREE.Vector3(x[0], x[1], x[2]))
+  let lines = new THREE.LineSegments(geo, frameMat)
+  lines.renderOrder = 999
+
+  let group = new THREE.Group()
+  group.add(cube, lines)
+  return group
+}
+
+function makeSphere(eigenv:THREE.Vector3): THREE.Object3D {
+  let n = eigenv.normalize()
+  var geometry = new THREE.SphereGeometry(1,22,15)
+  geometry.colors = geometry.vertices.map(v=>{
+    let red = Math.pow(Math.abs(n.dot(v.normalize())), 10)
+    return new THREE.Color(1 * red + 0.8 * (1-red),0.8 * (1-red),0.8 * (1-red))
+  })
+  var material = new THREE.PointsMaterial({
+    vertexColors: THREE.VertexColors,
+    size: 0.1
+  })
+  let sphere = new THREE.Points(geometry, material)
+  return sphere
+}
+
+function makeVector(v:THREE.Vector3, color: number): THREE.Object3D {
+
+  let coneGeo = new THREE.ConeGeometry(0.15, 0.3)
+  let coneMat = new THREE.MeshBasicMaterial({ color: color })
+  let cone = new THREE.Mesh(coneGeo, coneMat).translateY(2)
+
+  var linemat = new THREE.LineBasicMaterial({ color: color, linewidth: 3.5 });
+  var lineGeometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 2, 0)]);
+  var line = new THREE.Line(lineGeometry, linemat);
+
+  let group = new THREE.Group()
+  group.add(cone, line)
+  let q = new THREE.Quaternion()
+  q.setFromUnitVectors(new THREE.Vector3(0, 1, 0), v.normalize())
+  group.setRotationFromQuaternion(q)
+  group.renderOrder = 500
+  return group
+}
--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import * as THREE from 'three';
 import { Store, select } from '@ngrx/store';
-import { State, selectThreeMatrix, selectEverything, Shape, selectEigen, selectMatrix, selectShape } from '../reducers';
-import { Matrix4, Mesh, Geometry, LineSegments, Vector3, CylinderGeometry, Object3D, ConeGeometry, MeshBasicMaterial, Quaternion, Group, Sphere, SphereGeometry, Line } from 'three';
+import { State, selectModels } from '../reducers';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-render',
@@ -33,13 +31,6 @@ export class RenderComponent implements OnInit {
     this.renderer.setSize(512, 512);
     this.ref.nativeElement.appendChild(this.renderer.domElement);
 		
-
-    
-    // var controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
-    // // controls.addEventListener( 'change', render ); // use this only if there is no animation loop
-    // controls.enableZoom = false;
-    // controls.enablePan = false;
-
     this.camera.position.z = 5;
     this.camera.position.y = 2
     this.camera.position.x = 2
@@ -50,37 +41,12 @@ export class RenderComponent implements OnInit {
     this.scene.add(makeGrid())
 
     this.scene.add(makeAxis(0, 100, 0, 0x999999));
-    this.store.pipe(select(selectEverything)).subscribe(everything => {
-      let matrix = everything.matrix
-      let eigen = everything.ei
-      this.removeObjectsWithName("shape")
-      this.removeObjectsWithName("eigenvector")
 
-      let aaa = [0, 1, 2]
-      let eigenVectors = aaa.map(i=>everything.ei.eigenvectorMatrix.getColumn(i)).map(a=>new Vector3(a[0],a[1],a[2]))
-      let eigenColors = [0xff0000, 0x00ff00, 0x0000ff]//matches with console eigen font colors
-
-      let eigens = aaa.map(i => {
-        let col = eigen.eigenvectorMatrix.getColumn(i)
-        let v = makeVector(col[0], col[1], col[2], eigenColors[i])
-        v.name = "eigenvector"
-        return v
-      })
-      this.scene.add(...eigens)
-
-      let sh: Object3D
-      if (everything.sh == Shape.Cube) {
-        sh = makeCube()
-      } else if (everything.sh == Shape.Urchin) {
-        sh = makeSphere(eigenVectors[0])
-      } else {
-        sh = makeCube()
-      }
-      sh.applyMatrix4(matrix)
-      sh.name = "shape"
-      this.scene.add(sh)
-
-      this.renderer.render(this.scene, this.camera);
+    this.store.pipe(select(selectModels)).subscribe(models => {
+      this.removeObjectsWithName("model")
+      models.forEach(x=>{x.name = "model"})
+      this.scene.add(...models)
+      this.render()
     })
   }
   removeObjectsWithName(name: string) {
@@ -102,48 +68,6 @@ function makeAxis(x: number, y: number, z: number, color: number): THREE.Object3
   return line
 }
 
-function makeVector(x: number, y: number, z: number, color: number): THREE.Object3D {
-
-  let coneGeo = new ConeGeometry(0.15, 0.3)
-  let coneMat = new MeshBasicMaterial({ color: color })
-  let cone = new Mesh(coneGeo, coneMat).translateY(2)
-
-  var linemat = new THREE.LineBasicMaterial({ color: color, linewidth: 3.5 });
-  var lineGeometry = new THREE.BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 2, 0)]);
-  var line = new THREE.Line(lineGeometry, linemat);
-
-  let group = new Group()
-  group.add(cone, line)
-  let q = new Quaternion()
-  q.setFromUnitVectors(new Vector3(0, 1, 0), new Vector3(x, y, z).normalize())
-  group.setRotationFromQuaternion(q)
-  group.renderOrder = 500
-  return group
-}
-
-function makeCube(): Object3D {
-
-  var geometry = new THREE.BoxGeometry().translate(0.5, 0.5, 0.5);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0x70a0d0,
-    transparent: true,
-    opacity: 0.5
-  });
-  var cube = new THREE.Mesh(geometry, material);
-
-  var frameMat = new THREE.LineBasicMaterial({ color: 0x112233, linewidth: 3 });
-  let geo = new Geometry()
-  geo.vertices = [[0, 1, 0], [1, 1, 0], [1, 1, 0], [1, 1, 1], [1, 1, 1], [0, 1, 1], [0, 1, 1], [0, 1, 0],
-  [0, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 1], [1, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 0],
-  [0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 1, 1]].map(x => new Vector3(x[0], x[1], x[2]))
-  let lines = new LineSegments(geo, frameMat)
-  lines.renderOrder = 999
-
-  let group = new Group()
-  group.add(cube, lines)
-  return group
-}
-
 function makeGrid(): THREE.Object3D {
   var size = 5;
   var divisions = 5;
@@ -151,19 +75,4 @@ function makeGrid(): THREE.Object3D {
   var gridHelper = new THREE.GridHelper(size, divisions).translateX(2.5).translateZ(2.5);
   gridHelper.renderOrder = -999
   return gridHelper
-}
-
-function makeSphere(eigenv:Vector3): Object3D {
-  let n = eigenv.normalize()
-  var geometry = new THREE.SphereGeometry(1,22,15)
-  geometry.colors = geometry.vertices.map(v=>{
-    let red = Math.pow(Math.abs(n.dot(v.normalize())), 10)
-    return new THREE.Color(1 * red + 0.8 * (1-red),0.8 * (1-red),0.8 * (1-red))
-  })
-  var material = new THREE.PointsMaterial({
-    vertexColors: THREE.VertexColors,
-    size: 0.1
-  })
-  let sphere = new THREE.Points(geometry, material)
-  return sphere
 }
