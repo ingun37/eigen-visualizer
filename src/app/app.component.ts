5ff6d3159b856450b39f418ed03d79945c0ee06d
ingun <ingun37@gmail.com>
2020-02-28 14:02:08 +0900

    complex, real eien values display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { State, selectThreeMatrix } from './reducers';
+import { State, selectThreeMatrix, selectEigen } from './reducers';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,12 +12,24 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'eigen';
   interpolatedMatrix: Observable<string[][]>
+  realEigenvalues: Observable<string[][]>
+  complexEigenvalues: Observable<string[][]>
   constructor(
     private store:Store<State>
   ) {
     this.interpolatedMatrix = store.pipe(select(selectThreeMatrix)).pipe(
       map(x => {
         return [0, 1, 2].map(i => [0, 1, 2].map(j => Number( x.elements[j * 4 + i].toFixed(2)).toString()))
+      })
+    )
+    this.realEigenvalues = store.pipe(select(selectEigen)).pipe(
+      map(x=>{
+        return [x.realEigenvalues.map(y=>Number(y.toFixed(2)).toString())]
+      })
+    )
+    this.complexEigenvalues = store.pipe(select(selectEigen)).pipe(
+      map(x=>{
+        return [x.imaginaryEigenvalues.map(y=>Number(y.toFixed(2)).toString())]
       })
     )
   }
